1fb39f48c1a6d0a55ba9445a832eb2ef08827f85
ramin709 <79803446+ramin709@users.noreply.github.com>
2025-02-03 10:04:34 +0330

    Setup Kafka producer and consumer
    - Create a directory for Kafka files.
    - Create producer.js to set up Kafka producer and implement a send message function.
    - Create consumer.js to set up Kafka consumer.

---
+++ Back-end/kafka/consumer.js
@@ -0,0 +1,12 @@
+const kafka = require('kafka-node');
+
+const kafkaClient = new kafka.KafkaClient({ kafkaHost: process.env.KAFKA_HOST});
+const consumer = new kafka.Consumer(kafkaClient, [{topic: process.env.KAFKA_TOPIC, partition: 0}])
+
+consumer.on("message", async(message) => {
+    console.log("consumer is ready");
+})
+
+consumer.on("error", error => {
+    console.log("An error has occurred in consumer ", error)
+})
