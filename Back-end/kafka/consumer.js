942e5737f55beef8746f223c9685a0e27c652854
ramin709 <79803446+ramin709@users.noreply.github.com>
2025-02-03 10:16:56 +0330

    Setup socket
    - Create a directory for the sockets.
    - Create a file to setup Socket.
    - Add the "io" logic to the consumer.js

--- Back-end/kafka/consumer.js
+++ Back-end/kafka/consumer.js
@@ -1,10 +1,12 @@
 const kafka = require('kafka-node');
-
+const {io} = require('../socket/socketHandler.js')
 const kafkaClient = new kafka.KafkaClient({ kafkaHost: process.env.KAFKA_HOST});
 const consumer = new kafka.Consumer(kafkaClient, [{topic: process.env.KAFKA_TOPIC, partition: 0}])
 
 consumer.on("message", async(message) => {
     console.log("consumer is ready");
+
+    io.emit("newMessage", JSON.parse(message))    
 })
 
 consumer.on("error", error => {
